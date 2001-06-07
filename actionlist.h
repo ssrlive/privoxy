0b1c4671b3f88d0f8979af6ad898753405684f79
jongfoster <jongfoster@users.sourceforge.net>
2001-06-07 23:03:56 +0000

    Added standard comment at top of file.

--- actionlist.h
+++ actionlist.h
@@ -1,14 +1,48 @@
+/*********************************************************************
+ *
+ * File        :  $Source: /cvsroot/ijbswa/current/actionlist.h,v $
+ *
+ * Purpose     :  Master list of supported actions.
+ *                Not really a header, since it generates code.
+ *                This is included (3 times!) from actions.c
+ *                Each time, the following macros are defined to
+ *                suitable values beforehand:
+ *                    DEFINE_ACTION_MULTI()
+ *                    DEFINE_ACTION_STRING()
+ *                    DEFINE_ACTION_BOOL()
+ *                    DEFINE_ACTION_ALIAS
+ *
+ * Copyright   :  Written by and Copyright (C) 2001 the SourceForge
+ *                IJBSWA team.  http://ijbswa.sourceforge.net
+ *
+ *                Based on the Internet Junkbuster originally written
+ *                by and Copyright (C) 1997 Anonymous Coders and 
+ *                Junkbusters Corporation.  http://www.junkbusters.com
+ *
+ *                This program is free software; you can redistribute it 
+ *                and/or modify it under the terms of the GNU General
+ *                Public License as published by the Free Software
+ *                Foundation; either version 2 of the License, or (at
+ *                your option) any later version.
+ *
+ *                This program is distributed in the hope that it will
+ *                be useful, but WITHOUT ANY WARRANTY; without even the
+ *                implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *                PARTICULAR PURPOSE.  See the GNU General Public
+ *                License for more details.
+ *
+ *                The GNU General Public License should be included with
+ *                this file.  If not, you can view it at
+ *                http://www.gnu.org/copyleft/gpl.html
+ *                or write to the Free Software Foundation, Inc., 59
+ *                Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Revisions   :
+ *    $Log: actionlist.h,v $
+ *
+ *********************************************************************/
+
 
-/*
- * Master list of supported actions.
- *
- * Not really a header, since it generates code.
- *
- * This is included (3 times!) from actions.c
- * Each time, DEFINE_ACTION_MULTI(), DEFINE_ACTION_STRING(),
- * DEFINE_ACTION_BOOL(), and DEFINE_ACTION_ALIAS
- * are defined to suitable values beforehand.
- */
 DEFINE_ACTION_MULTI ("add-header",                              ACTION_MULTI_ADD_HEADER)
 DEFINE_ACTION_BOOL  ("block",           ACTION_BLOCK)
 DEFINE_ACTION_BOOL  ("fast-redirects",  ACTION_FAST_REDIRECTS)
