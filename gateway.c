14bf6e6f68da1df6e552fe67dfe936dc0faea315
steudten <steudten@users.sourceforge.net>
2001-09-12 17:58:26 +0000

    add #include <string.h>

--- gateway.c
+++ gateway.c
@@ -1,4 +1,4 @@
-const char gateway_rcs[] = "$Id: gateway.c,v 1.5 2001/07/29 18:47:57 jongfoster Exp $";
+const char gateway_rcs[] = "$Id: gateway.c,v 1.6 2001/09/10 10:41:16 oes Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/gateway.c,v $
@@ -34,6 +34,9 @@ const char gateway_rcs[] = "$Id: gateway.c,v 1.5 2001/07/29 18:47:57 jongfoster 
  *
  * Revisions   :
  *    $Log: gateway.c,v $
+ *    Revision 1.6  2001/09/10 10:41:16  oes
+ *    Added #include in.h
+ *
  *    Revision 1.5  2001/07/29 18:47:57  jongfoster
  *    Adding missing #include project.h
  *
@@ -67,6 +70,7 @@ const char gateway_rcs[] = "$Id: gateway.c,v 1.5 2001/07/29 18:47:57 jongfoster 
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <string.h>
 
 #ifdef _WIN32
 #include <winsock2.h>
