3406be3c7689cff97aa0c66323ad98f10190e98c
jongfoster <jongfoster@users.sourceforge.net>
2001-09-22 14:55:41 +0000

    Displaying an error message whenever the config directory is read.

---
+++ doc/webserver/config/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+<title>Internet JunkBuster not running</title>
+</head>
+
+<body>
+
+<h1>JunkBuster isn't working</h1>
+
+<p>If you can see this page, it means that you aren't using the
+JunkBuster
+proxy. &nbsp; Please check your browser settings.</p>
+
+<p><a href="http://ijbswa.sourceforge.net/">Junkbuster home page</a></p>
+
+</body>
+</html>
+
