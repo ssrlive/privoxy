2623262be8ad74f5f63025bfbcf02408f94c2bec
oes <oes@users.sourceforge.net>
2002-03-08 16:43:59 +0000

    Renamed cgi_transparent_png to cgi_transparent_image

--- cgisimple.h
+++ cgisimple.h
@@ -1,6 +1,6 @@
 #ifndef CGISIMPLE_H_INCLUDED
 #define CGISIMPLE_H_INCLUDED
-#define CGISIMPLE_H_VERSION "$Id: cgisimple.h,v 1.5 2002/01/22 23:26:03 jongfoster Exp $"
+#define CGISIMPLE_H_VERSION "$Id: cgisimple.h,v 1.6 2002/03/07 03:48:59 oes Exp $"
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/cgisimple.h,v $
@@ -38,6 +38,10 @@
  *
  * Revisions   :
  *    $Log: cgisimple.h,v $
+ *    Revision 1.6  2002/03/07 03:48:59  oes
+ *     - Changed built-in images from GIF to PNG
+ *       (with regard to Unisys patent issue)
+ *
  *    Revision 1.5  2002/01/22 23:26:03  jongfoster
  *    Adding cgi_transparent_gif() for http://i.j.b/t
  *
@@ -99,7 +103,7 @@ extern jb_err cgi_show_version (struct client_state *csp,
 extern jb_err cgi_show_request (struct client_state *csp,
                                 struct http_response *rsp,
                                 const struct map *parameters);
-extern jb_err cgi_transparent_png (struct client_state *csp,
+extern jb_err cgi_transparent_image (struct client_state *csp,
                                    struct http_response *rsp,
                                    const struct map *parameters);
 
