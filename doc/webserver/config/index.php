62f0e8380fdb62d3c4c1e1ba684504361e282185
jongfoster <jongfoster@users.sourceforge.net>
2002-03-25 23:13:51 +0000

    Renaming

--- doc/webserver/config/index.php
+++ doc/webserver/config/index.php
@@ -1,18 +1,21 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <html><head>
-<title>Internet JunkBuster not running</title>
+<title>Privoxy not running</title>
 </head>
 
 <body>
 
-<h1>JunkBuster isn't working</h1>
+<h1>Privoxy isn't working</h1>
 
-<p>If you can see this page, it means that you aren't using the
-JunkBuster
-proxy. &nbsp; Please check your browser settings.</p>
+<p>If you were redirected to this page, it means that you aren't using
+<a href="http://privoxy.org/">Privoxy</a>. &nbsp; Please check your browser settings.</p>
 
-<p><a href="http://ijbswa.sourceforge.net/">Junkbuster home page</a></p>
+<p>Junkbuster 2.9.x was renamed Privoxy.  If you recently upgraded, then
+the web-based interface has moved - it is now at <a href="http://config.privoxy.org/">http://config.privoxy.org/</a>
+(Short form: <a href="http://ijbswa.sourceforge.net/config/">p.p</a> [Privoxy Proxy]).</p>
+
+<p><a href="http://privoxy.org/">Privoxy home page</a></p>
 
 </body>
 </html>
