bc97cdad90304bd977372938a6bea1f5daaba665
swa <swa@users.sourceforge.net>
2002-04-03 19:41:11 +0000

    consistent look

--- doc/webserver/config/index.php
+++ doc/webserver/config/index.php
@@ -1,7 +1,45 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<!--
+
+  File        :  $Source: /cvsroot/ijbswa/current/doc/webserver/index.html,v $
+
+  Purpose     :  Warn user of incorrect configuration.
+                 This file belongs in
+                 ijbswa.sourceforge.net:/home/groups/i/ij/ijbswa/htdocs/
+
+  $Id: index.html,v 1.14 2002/03/27 09:34:26 swa Exp $
+
+  Written by and Copyright (C) 2001 the SourceForge
+  Privoxy team. http://www.privoxy.org/
+
+  Based on the Internet Junkbuster originally written
+  by and Copyright (C) 1997 Anonymous Coders and
+  Junkbusters Corporation.  http://www.junkbusters.com
+
+  This program is free software; you can redistribute it
+  and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at
+  your option) any later version.
+
+  This program is distributed in the hope that it will
+  be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  The GNU General Public License should be included with
+  this file.  If not, you can view it at
+  http://www.gnu.org/copyleft/gpl.html
+  or write to the Free Software Foundation, Inc., 59
+  Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+-->
+
 <html><head>
 <title>Privoxy not running</title>
+    <link rel="stylesheet" type="text/css" href="p_web.css">
 </head>
 
 <body>
@@ -11,11 +49,14 @@
 <p>If you were redirected to this page, it means that you aren't using
 <a href="http://privoxy.org/">Privoxy</a>. &nbsp; Please check your browser settings.</p>
 
-<p>Junkbuster 2.9.x was renamed Privoxy.  If you recently upgraded, then
-the web-based interface has moved - it is now at <a href="http://config.privoxy.org/">http://config.privoxy.org/</a>
-(Short form: <a href="http://ijbswa.sourceforge.net/config/">p.p</a> [Privoxy Proxy]).</p>
+<p>_Until version 2.9.13, Privoxy was also known as Internet
+Junkbuster. If you recently upgraded, then the web-based interface has
+moved - it is now at <a
+href="http://config.privoxy.org/">http://config.privoxy.org/</a>
+(Short form: <a href="http://ijbswa.sourceforge.net/config/">p.p</a>
+[Privoxy Proxy]).</p>
 
-<p><a href="http://privoxy.org/">Privoxy home page</a></p>
+<p>Please see the <a href="http://privoxy.org/">Privoxy home page</a>.</p>
 
 </body>
 </html>
