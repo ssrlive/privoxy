c679c7c5a038df89b3fb8fd1def9398fc054641c
Gabor Liptak <gliptak@gmail.com>
2002-04-05 00:56:09 +0000

    Correcting typo to clean up on realloc failure

--- loaders.c
+++ loaders.c
@@ -1,4 +1,4 @@
-const char loaders_rcs[] = "$Id: loaders.c,v 1.46 2002/03/24 13:25:43 swa Exp $";
+const char loaders_rcs[] = "$Id: loaders.c,v 1.47 2002/03/26 22:29:55 swa Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/loaders.c,v $
@@ -35,6 +35,9 @@ const char loaders_rcs[] = "$Id: loaders.c,v 1.46 2002/03/24 13:25:43 swa Exp $"
  *
  * Revisions   :
  *    $Log: loaders.c,v $
+ *    Revision 1.47  2002/03/26 22:29:55  swa
+ *    we have a new homepage!
+ *
  *    Revision 1.46  2002/03/24 13:25:43  swa
  *    name change related issues
  *
@@ -610,7 +613,7 @@ jb_err simple_read_line(FILE *fp, char **dest, int *newline)
       if (++len >= buflen)
       {
          buflen += BUFFER_SIZE;
-         if (NULL == (p = realloc(buf, buflen)));
+         if (NULL == (p = realloc(buf, buflen)))
          {
             free(buf);
             return JB_ERR_MEMORY;
