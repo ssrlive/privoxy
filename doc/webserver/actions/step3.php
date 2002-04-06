a275fe3bae5ad66c91709c492a16eaeec28aac0a
swa <swa@users.sourceforge.net>
2002-04-06 15:54:08 +0000

    prework: list of what needs to be submitted to the tracker.

--- doc/webserver/actions/step3.php
+++ doc/webserver/actions/step3.php
@@ -8,9 +8,12 @@
               This file belongs in
               ijbswa.sourceforge.net:/home/groups/i/ij/ijbswa/htdocs/
 
-  $Id: step3.php,v 1.5 2002/04/02 07:22:43 oes Exp $
+  $Id: step3.php,v 1.10 2002/04/06 15:19:35 oes Exp $
 
   $Log: step3.php,v $
+  Revision 1.10  2002/04/06 15:19:35  oes
+  Clean-up, smarter handling of unreachable URLs
+
   Revision 1.5  2002/04/02 07:22:43  oes
   Cosmetics
 
@@ -224,6 +227,70 @@ switch ($problem)
 }        
             
 fclose($fp);
+
+// now fill the tracker on sf with all the necessary values:
+//
+// <FORM ACTION="/tracker/index.php?group_id=11118&atid=460288" METHOD="POST" enctype="multipart/form-data">
+// <INPUT TYPE="HIDDEN" NAME="func" VALUE="postadd">
+//
+// <SELECT NAME="category_id">
+// <OPTION VALUE="100">None</OPTION>
+// <OPTION VALUE="412811">filter: ad-incorrectly-blocked</OPTION>
+// <OPTION VALUE="412810">filter: ad-not-blocked</OPTION>
+// <OPTION VALUE="412814">filter: other problem</OPTION>
+// <OPTION VALUE="412812">filter: page-incorrectly-block</OPTION>
+// <OPTION VALUE="412813">filter: popups-incorrectly blo</OPTION>
+//
+//  <SELECT NAME="artifact_group_id">
+//  <OPTION VALUE="100">None</OPTION>
+//  <OPTION VALUE="195870">action file version 1.0</OPTION>
+//  <OPTION VALUE="195871">action file version 1.1</OPTION>
+//  <OPTION VALUE="195872">action file version 1.2</OPTION>
+//  <OPTION VALUE="195873">action file version 1.3</OPTION>
+//  <OPTION VALUE="195874">action file version 1.4</OPTION>
+//  <OPTION VALUE="195875">action file version 1.5</OPTION>
+//  <OPTION VALUE="195876">action file version 1.6</OPTION>
+//  <OPTION VALUE="195877">action file version 1.7</OPTION>
+//  <OPTION VALUE="195878">action file version 1.8</OPTION>
+//  <OPTION VALUE="195879">action file version 1.9</OPTION>
+//  <OPTION VALUE="195880">action file version 2.0</OPTION> 
+//
+// <SELECT NAME="assigned_to">
+// <OPTION VALUE="100">None</OPTION>
+// <OPTION VALUE="128588">bart1803</OPTION>
+// <OPTION VALUE="249980">david__schmidt</OPTION>
+// <OPTION VALUE="79346">gliptak</OPTION>
+// <OPTION VALUE="322640">hal9</OPTION>
+// <OPTION VALUE="108873">joergs</OPTION>
+// <OPTION VALUE="199289">jongfoster</OPTION>
+// <OPTION VALUE="10857">morcego</OPTION>
+// <OPTION VALUE="78811">oes</OPTION>
+// <OPTION VALUE="43129">roro</OPTION>
+// <OPTION VALUE="249832">steudten</OPTION>
+// <OPTION VALUE="74317">swa</OPTION>
+//
+// <SELECT NAME="priority">
+// <OPTION VALUE="1">1 - Lowest</OPTION>
+// <OPTION VALUE="2">2</OPTION>
+// <OPTION VALUE="3">3</OPTION>
+// <OPTION VALUE="4">4</OPTION>
+// <OPTION VALUE="5" SELECTED>5 - Medium</OPTION>
+// <OPTION VALUE="6">6</OPTION>
+// <OPTION VALUE="7">7</OPTION>
+// <OPTION VALUE="8">8</OPTION>
+// <OPTION VALUE="9">9 - Highest</OPTION>
+//
+// 
+// we can either put everything in here (nicely formatted) ..
+// <TEXTAREA NAME="details" ROWS="30" COLS="55" WRAP="HARD"></TEXTAREA>
+//
+// ... or attach a file (with the html header so it gets displayed
+// as if it is a html file
+// <input type="checkbox" name="add_file" VALUE="1">
+// <input type="file" name="input_file" size="30">
+// <input type="text" name="file_description" size="40" maxlength="255">
+//
+// <INPUT TYPE="SUBMIT" NAME="SUBMIT" VALUE="SUBMIT">
 
 ?>
 
