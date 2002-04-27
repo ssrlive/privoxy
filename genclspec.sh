fd5b94761ebe22252751740799d652a540897b22
morcego <morcego@users.sourceforge.net>
2002-04-27 04:49:11 +0000

    Adding license and copyright comments.

--- genclspec.sh
+++ genclspec.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+#
+# $Id: genclspec,v 1.30 2002/04/26 15:51:05 morcego Exp $
+#
+# Written by and Copyright (C) 2001 the SourceForge
+# Privoxy team. http://www.privoxy.org/
+#
+# This program is free software; you can redistribute it 
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# The GNU General Public License should be included with
+# this file.  If not, you can view it at
+# http://www.gnu.org/copyleft/gpl.html
+# or write to the Free Software Foundation, Inc., 59
+# Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Log: genclspec.spec,v $
+#
 
 VERSION=`cat privoxy-rh.spec | sed -n -e 's/^Version:[ ]*//p'`
 RELEASE=`cat privoxy-rh.spec | sed -n -e 's/^Release:[ ]*//p'`
