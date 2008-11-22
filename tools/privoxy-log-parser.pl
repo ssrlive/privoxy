87f5a2bd34a2708ce34d72a2025425e5ae1931da
Fabian Keil <fk@fabiankeil.de>
2008-11-22 11:56:11 +0000

    Highlight: Created new connection to www.privoxy.org:80 on socket 11.

--- tools/privoxy-log-parser.pl
+++ tools/privoxy-log-parser.pl
@@ -8,7 +8,7 @@
 #
 # http://www.fabiankeil.de/sourcecode/privoxy-log-parser/
 #
-# $Id: privoxy-log-parser.pl,v 1.123 2008/11/09 19:21:23 fk Exp $
+# $Id: privoxy-log-parser.pl,v 1.124 2008/11/22 10:02:52 fk Exp $
 #
 # TODO:
 #       - LOG_LEVEL_CGI, LOG_LEVEL_ERROR, LOG_LEVEL_WRITE content highlighting
@@ -1422,6 +1422,12 @@ sub handle_loglevel_connect ($) {
     } elsif ($c =~ m/^socks5_connect:/) {
     
         $c =~ s@(?<=socks5_connect: )(.*)@$h{'error'}$1$h{'Standard'}@;
+
+    } elsif ($c =~ m/^Created new connection to/) {
+
+        # Created new connection to www.privoxy.org:80 on socket 11.
+        $c = highlight_matched_host($c, '(?<=connection to )[^\s]+');
+        $c =~ s@(?<=on socket )(\d+)@$h{'Number'}$1$h{'Standard'}@;
 
     } elsif ($c =~ m/^Found reusable socket/) {
 
