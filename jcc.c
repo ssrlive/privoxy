064cb55ff9f7584ccc1af9a4f0d77442c098d1c2
Fabian Keil <fk@fabiankeil.de>
2009-06-08 16:50:35 +0000

    The fflush() argument is a pointer. Don't use 0 instead of NULL.

--- jcc.c
+++ jcc.c
@@ -1,4 +1,4 @@
-const char jcc_rcs[] = "$Id: jcc.c,v 1.251 2009/06/03 16:42:49 fabiankeil Exp $";
+const char jcc_rcs[] = "$Id: jcc.c,v 1.252 2009/06/08 16:48:50 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/jcc.c,v $
@@ -1819,7 +1819,7 @@ static void chat(struct client_state *csp)
        */
       if (FD_ISSET(csp->sfd, &rfds))
       {
-         fflush(0);
+         fflush(NULL);
          len = read_socket(csp->sfd, buf, sizeof(buf) - 1);
 
          if (len < 0)
