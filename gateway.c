8c3b1a6ae00b5609dfa74ae7c0c98981991ee05b
Fabian Keil <fk@fabiankeil.de>
2009-09-22 11:35:52 +0000

    Fix a log message in get_reusable_connection(). A blank was missing.

--- gateway.c
+++ gateway.c
@@ -1,4 +1,4 @@
-const char gateway_rcs[] = "$Id: gateway.c,v 1.60 2009/09/06 14:15:46 fabiankeil Exp $";
+const char gateway_rcs[] = "$Id: gateway.c,v 1.61 2009/09/10 14:59:34 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/gateway.c,v $
@@ -506,7 +506,7 @@ static jb_socket get_reusable_connection(const struct http_request *http,
             reusable_connection[slot].in_use = TRUE;
             sfd = reusable_connection[slot].sfd;
             log_error(LOG_LEVEL_CONNECT,
-               "Found reusable socket %d for %s:%d in slot %d."
+               "Found reusable socket %d for %s:%d in slot %d. "
                "Timestamp made %d seconds ago. Timeout: %d. Latency: %d.",
                sfd, reusable_connection[slot].host, reusable_connection[slot].port,
                slot, time(NULL) - reusable_connection[slot].timestamp,
