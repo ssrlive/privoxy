dd14a1de75e6f54bdbdbb617e51dd1c04cd9f6bd
Fabian Keil <fk@fabiankeil.de>
2009-10-31 10:14:07 +0000

    Accept: keep-alive support is disabled. Crunching: Keep-Alive: 300.

--- tools/privoxy-log-parser.pl
+++ tools/privoxy-log-parser.pl
@@ -8,7 +8,7 @@
 #
 # http://www.fabiankeil.de/sourcecode/privoxy-log-parser/
 #
-# $Id: privoxy-log-parser.pl,v 1.190 2009/10/10 05:29:26 fk Exp $
+# $Id: privoxy-log-parser.pl,v 1.57 2009/10/10 05:50:02 fabiankeil Exp $
 #
 # TODO:
 #       - LOG_LEVEL_CGI, LOG_LEVEL_ERROR, LOG_LEVEL_WRITE content highlighting
@@ -918,6 +918,7 @@ sub handle_loglevel_header ($) {
           or $c =~ m/Content modified with no Content-Length header set/
           or $c =~ m/^Appended client IP address to/
           or $c =~ m/^Removing 'Connection: close' to imply keep-alive./
+          or $c =~ m/^keep-alive support is disabled/
             )
     {
         # XXX: Some of these may need highlighting
@@ -962,6 +963,7 @@ sub handle_loglevel_header ($) {
         # Content modified with no Content-Length header set. Creating a fake one for adjustment later on.
         # Appended client IP address to X-Forwarded-For: 10.0.0.2, 10.0.0.1
         # Removing 'Connection: close' to imply keep-alive.
+        # keep-alive support is disabled. Crunching: Keep-Alive: 300.
 
     } elsif ($c =~ m/^scanning headers for:/) {
 
