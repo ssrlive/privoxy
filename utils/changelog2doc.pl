72b847e8c4a1ad91cb47f2981b973e6d458ea5a2
Fabian Keil <fk@fabiankeil.de>
2010-10-31 13:29:31 +0000

    Add a license header.

--- utils/changelog2doc.pl
+++ utils/changelog2doc.pl
@@ -1,11 +1,27 @@
 #!/usr/bin/perl
 
-# $Id: changelog2doc.pl,v 1.5 2010/10/31 13:27:03 fabiankeil Exp $
-# $Source: /cvsroot/ijbswa/current/utils/changelog2doc.pl,v $
-
+##########################################################################
+#
+# $Id: changelog2doc.pl,v 1.2 2008/09/26 16:49:09 fabiankeil Exp $
+#
 # Filter to parse the ChangeLog and translate the changes for
 # the most recent version into something that looks like markup
 # for the documentation but still needs fine-tuning.
+#
+# Copyright (c) 2008, 2010 Fabian Keil <fk@fabiankeil.de>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+##########################################################################
 
 use strict;
 use warnings;
