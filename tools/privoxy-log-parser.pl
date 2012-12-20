6608b2b05edd3f6ea2832bb76be5fccc025914e0
Fabian Keil <fk@fabiankeil.de>
2012-12-20 17:02:30 +0000

    Accept and highlight: Shifting 360 pipelined bytes by 360 bytes

--- tools/privoxy-log-parser.pl
+++ tools/privoxy-log-parser.pl
@@ -8,7 +8,7 @@
 #
 # http://www.fabiankeil.de/sourcecode/privoxy-log-parser/
 #
-# $Id: privoxy-log-parser.pl,v 1.144 2012/10/29 11:59:08 fabiankeil Exp $
+# $Id: privoxy-log-parser.pl,v 1.145 2012/12/07 12:46:04 fabiankeil Exp $
 #
 # TODO:
 #       - LOG_LEVEL_CGI, LOG_LEVEL_ERROR, LOG_LEVEL_WRITE content highlighting
@@ -1723,6 +1723,12 @@ sub handle_loglevel_connect ($) {
 
         # Tainting client socket 7 due to unread data.
         $c =~ s@(?<=socket )(\d+)@$h{'Number'}$1$h{'Standard'}@;
+
+    } elsif ($c =~ m/^Shifting \d+ pipelined bytes/) {
+
+        # Shifting 360 pipelined bytes by 360 bytes
+        $c =~ s@(?<=Shifting )(\d+)@$h{'Number'}$1$h{'Standard'}@;
+        $c =~ s@(?<=by )(\d+)@$h{'Number'}$1$h{'Standard'}@;
 
     } elsif ($c =~ m/^Looks like we / or
              $c =~ m/^Unsetting keep-alive flag/ or
