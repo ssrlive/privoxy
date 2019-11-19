94a348028a8d9b04f1fb60d1dc0e9e31e6d2758d
Fabian Keil <fk@fabiankeil.de>
2019-11-19 14:33:09 +0100

    Add a missing space

--- project.h
+++ project.h
@@ -354,7 +354,7 @@ typedef struct certs_chain {
    char text_buf[CERT_INFO_BUF_SIZE];    /* text info about properties of certificate               */
    char file_buf[CERT_FILE_BUF_SIZE];    /* buffer for whole certificate - format to save in file   */
    struct certs_chain *next;             /* next certificate in chain of trust                      */
-}certs_chain_t;
+} certs_chain_t;
 #endif
 
 /**
