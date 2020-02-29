3bfbbaeeeef212763d53b89821e264e7fc97da74
Fabian Keil <fk@fabiankeil.de>
2020-02-29 09:49:39 +0100

    finish_http_response(): Plug memory leak with CORS enabled
    Introduced in 9fd58c0d, not in any release.
    
    Fixes CID 267166 "Resource leaks".

--- cgi.c
+++ cgi.c
@@ -1594,7 +1594,7 @@ struct http_response *finish_http_response(struct client_state *csp, struct http
    if (NULL != csp->config->cors_allowed_origin)
    {
       enlist_unique_header(rsp->headers, "Access-Control-Allow-Origin",
-         strdup_or_die(csp->config->cors_allowed_origin));
+         csp->config->cors_allowed_origin);
       enlist_unique_header(rsp->headers, "Access-Control-Allow-Methods", "GET,POST");
       enlist_unique_header(rsp->headers, "Access-Control-Allow-Headers", "X-Requested-With");
       enlist_unique_header(rsp->headers, "Access-Control-Max-Age", "86400");
