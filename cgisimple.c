dea66ab08a60bd782ac846b623655bb1fc1e2e5a
Fabian Keil <fk@fabiankeil.de>
2020-05-28 11:00:43 +0200

    Fix comment typos

--- cgisimple.c
+++ cgisimple.c
@@ -1365,7 +1365,7 @@ jb_err cgi_show_url_info(struct client_state *csp,
 
    if (url_param[0] == '\0')
    {
-      /* URL paramater not specified, display query form only. */
+      /* URL parameter not specified, display query form only. */
       free(url_param);
       if (map_block_killer(exports, "url-given")
         || map(exports, "url", 1, "", 1))
@@ -1714,7 +1714,7 @@ jb_err cgi_robots_txt(struct client_state *csp,
  *
  * Function    :  show_defines
  *
- * Description :  Add to a map the state od all conditional #defines
+ * Description :  Add to a map the state of all conditional #defines
  *                used when building
  *
  * Parameters  :
