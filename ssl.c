a473a2a85dbf5325b270a906e85785ba4b032503
Fabian Keil <fk@fabiankeil.de>
2021-01-22 09:13:14 +0100

    MbedTLS: Use mbedtls_md5_ret() instead of mbedtls_md5()
    As Roland pointed out the latter has been deprecated
    years ago and causes a warning on Debian GNU/Linux.

--- ssl.c
+++ ssl.c
@@ -1780,8 +1780,15 @@ static int host_to_hash(struct client_state *csp)
 #error mbedTLS needs to be compiled with md5 support
 #else
    memset(csp->http->hash_of_host, 0, sizeof(csp->http->hash_of_host));
-   mbedtls_md5((unsigned char *)csp->http->host, strlen(csp->http->host),
-      csp->http->hash_of_host);
+   ret = mbedtls_md5_ret((unsigned char *)csp->http->host,
+      strlen(csp->http->host), csp->http->hash_of_host);
+   if (ret != 0)
+   {
+      log_error(LOG_LEVEL_ERROR,
+         "Failed to generate md5 hash of host %s: %d",
+         csp->http->host, ret);
+      return -1;
+   }
 
    /* Converting hash into string with hex */
    size_t i = 0;
