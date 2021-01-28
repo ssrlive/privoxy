f5c1a886b7ae20da7eafb77926252eb521260728
Fabian Keil <fk@fabiankeil.de>
2021-01-28 16:26:45 +0100

    decompress_iob(): Fix detection of insufficient data
    Instead of checking the size of the iob we have to
    check the size of the actual data.
    
    Previously Privoxy could try to work on uninitialized data.

--- parsers.c
+++ parsers.c
@@ -540,7 +540,7 @@ jb_err decompress_iob(struct client_state *csp)
 
    cur = csp->iob->cur;
 
-   if (bufsize < (size_t)10)
+   if (old_size < (size_t)10)
    {
       /*
        * This is to protect the parsing of gzipped data,
