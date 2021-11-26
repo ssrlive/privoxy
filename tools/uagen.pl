ssrlive/privoxy

commit 077333a08ded8a30a003596b40387ede2e3c6cf8

uagen: Bump generated Firefox version to 91 (ESR)

tools/uagen.pl
@@ -59,8 +59,8 @@ use constant {
    # are too lazy to check, but want to change them anyway, take the values you
    # see in the "Help/About Mozilla Firefox" menu.
 
-   BROWSER_VERSION                   => "78.0",
-   BROWSER_REVISION                  => '78.0',
+   BROWSER_VERSION                   => "91.0",
+   BROWSER_REVISION                  => '71.0',
    BROWSER_RELEASE_DATE              => '20100101',
 };
 

commit 874ab06d5d2a142f29241ff9d8270e3b9f7dd06c

uagen: Bump BROWSER_REVISION to match Firefox version to 91 (ESR)
Regression introduced in 077333a08de.

tools/uagen.pl
@@ -60,7 +60,7 @@ use constant {
    # see in the "Help/About Mozilla Firefox" menu.
 
    BROWSER_VERSION                   => "91.0",
-   BROWSER_REVISION                  => '71.0',
+   BROWSER_REVISION                  => '91.0',
    BROWSER_RELEASE_DATE              => '20100101',
 };
 