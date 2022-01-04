f982473cab71fd3a7e4cbb06aec44c94c480f225
Fabian Keil <fk@fabiankeil.de>
2022-01-04 22:02:39 +0100

    uagen: Stop using sparc64 as FreeBSD architecture
    It hasn't been supported for a while now.

--- tools/uagen.pl
+++ tools/uagen.pl
@@ -138,7 +138,7 @@ sub generate_platform_and_os() {
         FreeBSD => {
             karma             => 1,
             platform          => 'X11',
-            architectures     => [ 'i386', 'amd64', 'sparc64' ],
+            architectures     => [ 'i386', 'amd64' ],
             order_is_inversed => 0,
         },
         OpenBSD => {
