e32d03e08581b292864d70768c6172e42e979b1c
Fabian Keil <fk@fabiankeil.de>
2023-09-09 08:55:33 +0200

    Add missing client-body-tagger data to the action_type_info[] struct
    ... so lookups based on the action index work correctly again.
    
    Prevents assertion failures or segfaults when trying to edit
    an action file with the CGI editor.
    
    The type of failure depended on whether or not assertions
    were enabled and on whether or not Privoxy had been compiled
    with FEATURE_EXTERNAL_FILTERS.
    
    Regression introduced in Privoxy 3.0.34.
    
    Patch submitted by Aaron Li in #940.

--- cgiedit.c
+++ cgiedit.c
@@ -253,6 +253,12 @@ static const struct action_type_info action_type_info[] =
       "P", "CLIENT-BODY-FILTER"
    },
    {
+      ACTION_MULTI_CLIENT_BODY_TAGGER,
+      "client-body-tagger-params", "client-body-tagger",
+      "client-body-tagger-all", "client_body_tagger_all",
+      "Q", "CLIENT-BODY-TAGGER"
+   },
+   {
       ACTION_MULTI_ADD_HEADER,
       "add-header-params", "add-header",
       "add-header-all", "add_header_all",
