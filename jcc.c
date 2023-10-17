99d7471c05f3d380a624d260a75538fbb4a27107
Fabian Keil <fk@fabiankeil.de>
2023-10-17 18:44:08 +0200

    Bump copyright

--- jcc.c
+++ jcc.c
@@ -5,7 +5,7 @@
  * Purpose     :  Main file.  Contains main() method, main loop, and
  *                the main connection-handling function.
  *
- * Copyright   :  Written by and Copyright (C) 2001-2022 the
+ * Copyright   :  Written by and Copyright (C) 2001-2023 the
  *                Privoxy team. https://www.privoxy.org/
  *
  *                Based on the Internet Junkbuster originally written
